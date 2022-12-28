e3147701ce8ba7f8f5d0ea789b14dcab00cd470b
Zjian777 <zhouzijian8974@163.com>
2022-12-28 14:13:57 +0800

    first commit

---
+++ spider.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.x-mol.com/paper/search/q?option=coronavirus"
+urlcode = requests.get(url)
+soup = BeautifulSoup(urlcode.text,"lxml")
+tag = soup.find_all('div',class_="it-bold space-bottom-m10")
+for i in tag:
+	with open(file = "/root/code/output.txt",mode = 'a',encoding ='utf-8')as f:
+		text = i.get_text().replace("\n","")
+		f.write(text.strip())
+		f.write("\n")
+
